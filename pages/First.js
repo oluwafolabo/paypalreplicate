2c777a32ec10ba801f6bd0ca21204c27e3cc8a32
oluwafolabo <oluwayalefolabo@gmail.com>
2021-09-15 23:16:16 +0100

    First Commit

---
+++ pages/First.js
@@ -0,0 +1,196 @@
+import Image from 'next/image'
+import PayPal from '../public/PayPal.png'
+import styles from "../styles/first.module.css"
+import Link from"next/link"
+import Head from"next/head"
+import Background from "../public/background.jpg"
+const First = () => {
+    const itCloseUp=()=>{
+
+         
+   
+    }
+    return (
+        <div>
+             <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;900&display=swap" 
+                rel="stylesheet"/>
+            </Head>
+            <div>
+                <nav className={styles.nav}>
+                    <div className= {styles.menu}>
+                    <Link href="/"><a>Menu</a></Link>
+                    </div>
+                    
+                    <div className={styles.logo} onClick={itCloseUp}>
+                        <Image src={PayPal} alt="logo" width={90} height={30}/>
+                    </div>
+                    <div className={styles.PERSONAL} onMouseOver={itCloseUp}>
+                    <Link href="/"><a>PERSONAL</a></Link>
+                    </div >
+                    <ul>
+                     
+                        <Link href="/" ><a className={styles.login}>Log In</a></Link>
+                       
+                        <Link href="/"><a className={styles.signup}>Sign Up</a></Link>
+                    </ul>
+
+                </nav>
+
+                <div className={styles.dropdown}>
+
+                </div>
+                <div className={styles.dropdown1}>
+
+                     <Image src={Background} alt="logo" width={1500} height={400}className={styles.Background} />
+                     <div className={styles.overLay}>
+                         <h1 className={styles.FirstLine}>The simpler, safer way to pay online.</h1>
+                         <div className={styles.signFree}>
+                         <Link href="/"><a>Sign Up for Free</a></Link>
+                        
+                         </div>
+                         <div className={styles.own}>
+                         <h1>Own a business?
+                              <Link href="/"><a className={styles.signup}> Open a Business Account
+                        </a></Link></h1>
+                        </div>
+                     </div>
+                </div>
+                
+               {/*bod*/}
+
+               <div className={styles.paypal}>
+                   <h1> Paypal is for everyone who pays is online</h1>
+                   <div className={styles.icon} onClick={itCloseUp}>
+                        <Image src={PayPal} alt="logo" width={90} height={30}/>
+                    </div>
+                    <div className={styles.Individual}>
+                        <h1> Individuals</h1>
+                    </div>
+                    <div className={styles.Find}>
+                        <h1> Find out why we have more than 200M active accounts worldwide.</h1>
+                    </div>
+                    <div className={styles.Learn} >
+                        <Link href="/"><a className={styles.signup}>Learn More</a></Link>
+                    </div>
+               </div>
+
+                {/* another body*/}
+                
+                <div className={styles.flex}>
+                    <h1 className={styles.connect}> Paypal connects buyers and sellers</h1>
+                    <div className={styles.buyers}>
+                        <h1>For Buyers</h1>
+                    </div>
+
+                    {/* flexbox*/}
+                    <div className={styles.flexbox}>
+                        {/* flexbox1*/}
+                        <div className={styles.flexbox1}>
+                            <div>
+                                <Image src={PayPal} alt="logo" width={90} height={30}/>
+                            </div>
+                            <div className={styles.line}>
+                                <div className={styles.number}>
+                                    <h1>1</h1>
+                                </div>
+                                <div>
+                                    
+                                    <h1>
+                                    <Link href="/"><a className={styles.signup}> Sign Up
+                                    </a></Link> with just an email address and password</h1>
+                                </div>
+                            </div>
+                        </div>
+                              {/* flexbox2*/}
+                        <div className={styles.flexbox2}>
+                            <div>
+                                <Image src={PayPal} alt="logo" width={90} height={30}/>
+                            </div>
+                            <div className={styles.line}>
+                                <div className={styles.number}>
+                                    <h1>2</h1>
+                                </div>
+                                <div>
+                                    
+                                    <h1>Secure add your card</h1>
+                                </div>
+                            </div>
+                        </div>
+                         {/* flexbox3*/}
+                         <div className={styles.flexbox3}>
+                            <div className={styles.image}>
+                                <Image src={PayPal} alt="logo" width={90} height={30}/>
+                            </div>
+                            <div className={styles.line}>
+                                <div className={styles.number}>
+                                    <h1>3</h1>
+                                </div>
+                                <div>
+                                     <h1>Use the paypal button check out with just an email address and password</h1>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div className={styles.learnMore}>
+                        <h1><Link href="/">Learn More</Link></h1>
+                    </div>
+                </div>
+
+                 {/* flex2*/}
+
+                <div className={styles.flex2}>
+                    <h1 className={styles.connect}>Join 200M active paypal accounts worldwide</h1>
+                    <div className={styles.box}>
+                        <div className={styles.box1}>
+                            <h1 className={styles.boxTop}>Safer and protected</h1>
+                            <h1 className={styles.boxTop1}>Shop with peace of mind. We don’t share your full financial information with sellers. And PayPal Buyer Protection 
+                                covers your eligible purchases if they don’t show up or match their description.</h1>
+                            <h1 className={styles.boxTop2}><Link href="/">More about security</Link></h1>   
+                         </div>
+                             {/* box2*/}
+                         <div className={styles.box2}>
+                            <h1 className={styles.boxTop}>Simple and convenient</h1>
+                            <h1 className={styles.boxTop1}>Pay online with your PayPal password and skip entering your financial information. Or pay even faster with One Touch™, stay logged in and check out without entering your password.
+                                Plus, rest assured knowing Buyer Protection is available for your eligible purchases.</h1>
+                            <h1 className={styles.boxTop2}><Link href="/">More about One Touch™</Link></h1>   
+                         </div>
+                             {/* box3*/}
+                             <div className={styles.box3}>
+                            <h1 className={styles.boxTop}>Mostly free, always upfront</h1>
+                            <h1 className={styles.boxTop1}>It’s free to open a PayPal account and buy something using 
+                                PayPal unless it involves a currency conversion.</h1>
+                            <h1 className={styles.boxTop2}><Link href="/">More about security</Link></h1>   
+                         </div>
+                    </div>
+                </div>
+                 {/* flex3*/}
+
+                 <div className={styles.started}>
+                     <h1>Sign up and get started.</h1>
+                     <div className={styles.startedButton}><Link href="/"><a>Get Started</a></Link></div>
+                 </div>
+
+                  {/* flex3*/}
+                  <div className={styles.footer}>
+                        <div className={styles.footerLink}>
+                            <Link href="/" ><a className={styles.login}> Help and Contact</a></Link>
+                            <Link href="/"><a className={styles.signup}> Fees</a></Link>
+                            <Link href="/"><a className={styles.signup}> Security</a></Link>
+                            <Link href="/"><a className={styles.signup}> Shop</a></Link>
+                            <Link href="/"><a className={styles.signup}> Features</a></Link>
+                        </div>
+                        <div className={styles.footerImage}>
+                            <Image src={PayPal} alt="logo" width={90} height={30}/>
+                        </div>
+                       
+                  </div>
+                  <hr className={styles.hr}/>
+            </div>
+        </div>
+    )
+}
+export default First
---
+++ pages/Login.js
@@ -0,0 +1,64 @@
+import styles from "../styles/login.module.css"
+import Head from"next/head"
+import Link from"next/link"
+import Image from 'next/image'
+import PayPal from '../public/PayPal.png'
+import First from "./First"
+ export const Login = () => {
+        const getInput =() =>{
+            var text = document.getElementById("text").value;
+            console.log(text)
+        }
+    
+    return (
+        <div>
+            <Head>
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;900&display=swap" 
+                rel="stylesheet"/>
+            </Head>
+        <div className={styles.container}> 
+
+
+            <form className={styles.borders}>
+                <div className={styles.logo}>
+                    <Image src={PayPal} alt="logo" width={90} height={30}/>
+                </div>
+                <div className={styles.input}>
+                    <input id="text" type="Email" autoComplete="text" placeholder="Email or mobile number"/>
+                </div>
+                <div  className={styles.input1}>
+                    <input id="text" type="password" autoComplete="text" placeholder="Password"/>
+                </div>
+                <div className={styles.forget}>
+                    <a href="#">Forget Password?</a>
+                </div>
+                <Link href="/First">
+                <div className={styles.Login} onClick={getInput}>
+                   <a href="#"> Log In</a>
+                </div>
+                </Link>
+                <div className={styles.hrOr}>
+                    <hr className={styles.hr}/> 
+                    <hr className={styles.hr1}/>
+                   <div className={styles.or}>or</div>
+
+                </div>
+                
+                <div className={styles.SignUp}>
+                  <a href ="/">Sign Up</a>
+                  
+                </div>
+            </form>
+            
+            <div className={styles.footer}>
+                <div className={styles.footerName}>
+                    <a href="#">Contact Us</a>
+                    <a href="#">Privacy</a>
+                    <a href="#">Legal</a>
+                    <a href="#">Worldwide</a>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
